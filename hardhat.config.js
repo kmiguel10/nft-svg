e483e6070247b9071564412061815d8c3c45b880
Kent Miguel <kentmiguel10@gmail.com>
2022-05-18 23:13:45 -0400

    deployed to rinkeby testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
+require("dotenv").config();
+require("hardhat-deploy");
+require("@nomiclabs/hardhat-waffle");
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.7",
+  solidity: {
+    compilers: [{ version: "0.8.7" }, { version: "0.6.6" }],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 4,
+    },
+  },
 };
